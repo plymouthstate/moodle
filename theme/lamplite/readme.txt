679c5471f12df289ed9b6ccc04cf7ced6dbf98fa
Nathan Porter <nrporter1@gmail.com>
2011-07-28 08:28:42 -0400

    Adding LTOE requested themes to repository.

---
+++ theme/lamplite/readme.txt
@@ -0,0 +1,11 @@
+LAMPLITE - A MOODLE 2.0 THEME
+
+Lamplite has been created by Richard Oelmann based on the hard work of many other Moodle contributors
+
+The Lamplite theme is essentially a customisation of Patrick Malley's Serenity theme.
+To this basic theme, I have pulled in the profile block from Shaun Daubney's Aardvark theme series.
+I have also added in the coding for Moodle2 custom menus from the Moodle 2 Themes documentation.
+
+Given that I have 'played with' the various bits of php and css linked to all these sections I would also point out that any errors are my own!!!
+
+I would also like to acknowledge the support and advice of Mary Evans through the Moodle Themes forum
---
+++ theme/metal/lang/en/theme_metal.php
@@ -0,0 +1,34 @@
+<?php
+
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Strings for component 'theme_metal', language 'en', branch 'MOODLE_20_STABLE'
+ *
+ * @package   theme_standard
+ * @copyright 1999 onwards Martin Dougiamas  {@link http://moodle.com}
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+$string['pluginname'] = 'Metal';
+$string['region-side-post'] = 'Right';
+$string['region-side-pre'] = 'Left';
+$string['choosereadme'] = '<h2>MOODLE THEME: METAL</h2>
+<p>This theme is designed by A. Chavan (chavan at users.sourceforge.net).</p>
+<p>Metal Theme for Moodle 2.0 is based on Moodle 2.0 standard theme, and uses the same background image from Moodle 1.9 Metal theme, which was inspired by:
+<a href="http://www.alexking.org/software/wordpress/styles/sample.php?wpstyle=metaldreams">Metal Dreams - A WordPress Theme by Alex King</a></p>
+<p>UPDATED for Moodle 2.0 by Mary Evans</p>
+<h3>Lastupdate: Wed Dec  01 22:18:00 2010.</h3>';
