e2810376c2c6f96d79b71c8105770d21265ae9c6
Nathan Porter <nrporter1@gmail.com>
2011-12-02 14:01:50 -0500

    Adding Journal mod plugin.

---
+++ mod/journal/version.php
@@ -0,0 +1,8 @@
+<?php
+
+$module->version  = 2011110500;
+$module->requires = 2010112400;  // Requires this Moodle version
+$module->release = '1.1 (Build: 2011110500)';
+$module->maturity = MATURITY_STABLE;
+$module->cron     = 60;
+
