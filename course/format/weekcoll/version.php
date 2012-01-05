0bb63d0186d9236e781f22bd8970c2c1570f7276
nrporter <nrporter1@gmail.com>
2012-01-05 16:24:29 -0500

    Installing the Collapsed Week course format (version 2011101114).

---
+++ course/format/weekcoll/version.php
@@ -0,0 +1,33 @@
+<?php
+
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+ /**
+ * Collapsed Weeks Information
+ *
+ * @package    course/format
+ * @subpackage weekcoll
+ * @copyright  2009-2011 G J Barnard in respect to modifications of standard weeks format.
+ * @author     G J Barnard {@link http://moodle.org/user/profile.php?id=442195}
+ * @link       http://docs.moodle.org/en/Collapsed_Weeks_course_format
+ * @license    http://creativecommons.org/licenses/by-sa/3.0/ Creative Commons Attribution-ShareAlike 3.0 Unported (CC BY-SA 3.0)
+ */
+ 
+ $plugin->version = 2011101114;
+ $plugin->maturity = MATURITY_STABLE;
+ $plugin->requires = 2011070102.01; // 2.1.2
+ $plugin->component = 'format_weekcoll';
+ $plugin->release = '1.3.1';
