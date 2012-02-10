afe91743f395fbcda4d6d35bf32e1cff5f6d6450
nrporter <nrporter1@gmail.com>
2012-02-10 16:37:37 -0500

    Adding a plugin to alter the guest policy page that will be activated in production moodle.

---
+++ local/0/alter_guest_policy.js
@@ -0,0 +1,32 @@
+M.alter_guest_policy = {
+
+	Y: null,
+	pageid: null,
+
+	init: function(Y) {
+		this.Y = Y;
+		this.pageid = Y.one('body').get('id');
+
+		if( this.pageid == 'page-user-policy' ) {
+			M.alter_guest_policy.mod_buttons( this.pageid );
+		}
+	},
+
+	mod_buttons: function( pageid ) {
+		Y = this.Y;
+		Y.one('.noticebox').set('hidden', true);
+		var notice_msg = Y.one('#notice p');
+		notice_msg.set('text', 'Moodle has recognized you as a guest. If this is correct, select the “Continue as Guest” button.');
+		var el = Y.Node.create('<p>If you are a PSU user, select the “Login” button to enter your username and password.</p>');
+		el.appendTo(notice_msg);
+
+		Y.all('#notice .buttons form').each( function() {
+			if( this.get('action').indexOf('policy.php') != -1 ) {
+				this.one('input').set('value', 'Continue as Guest');
+			} else {
+				this.set('action', 'http://www.plymouth.edu/webapp/courses2/login/index.php');
+				this.one('input').set('value', 'Login');
+			}
+		});
+	}
+}
--- local/0/lib.php
+++ local/0/lib.php
@@ -4,6 +4,12 @@
 	$jsmodule = array(
 		'name'  =>  'local_psu_mod',
 		'fullpath'  =>  '/local/0/prevent_destructive_restore.js',
-		'requires'  =>  array('base', 'node', 'io', 'json')
+		'requires'  =>  array('base', 'node', 'io', 'json'),
 	);
 	$PAGE->requires->js_init_call('M.local_psu_mod.init', null, true, $jsmodule);
+	$jsmod_aup = array(
+		'name' => 'alter_guest_policy',
+		'fullpath' => '/local/0/alter_guest_policy.js',
+		'requires'  =>  array('base', 'node', 'io', 'json'),
+	);
+	$PAGE->requires->js_init_call('M.alter_guest_policy.init', null, true, $jsmod_aup);
