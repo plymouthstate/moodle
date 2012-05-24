56ce68149727ec7744c375f5db840359b60d0152
nrporter <nrporter1@gmail.com>
2012-05-24 14:17:00 -0400

    Installing an updated verion of the Journal Module (Build 2012052100 Release 1.4)

--- mod/journal/version.php
+++ mod/journal/version.php
@@ -1,8 +1,8 @@
 <?php
 
-$module->version  = 2011110500;
-$module->requires = 2010112400;  // Requires this Moodle version
-$module->release = '1.1 (Build: 2011110500)';
+$module->version  = 2012052100;
+$module->requires = 2011070100;  // Moodle 2.1 (because of 1.9 course restore)
+$module->release = '1.4 (Build: 2012052100)';
 $module->maturity = MATURITY_STABLE;
 $module->cron     = 60;
 
