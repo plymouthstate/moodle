d6038857d3e0eeca64aee2ac0d935d1f2815e6c2
nrporter <nrporter1@gmail.com>
2012-05-25 08:02:44 -0400

    Updating the psu_darkb modified darkb theme to instead read as Plymouth State Green.

--- theme/psu_darkb/lang/en/theme_psu_darkb.php
+++ theme/psu_darkb/lang/en/theme_psu_darkb.php
@@ -1,9 +1,9 @@
 <?php
-$string['pluginname'] = 'psu_darkb';
+$string['pluginname'] = 'Plymouth State Green';
 $string['region-side-post'] = 'Right';
 $string['region-side-pre'] = 'Left';
-$string['choosereadme'] = '<div class="clearfix"><div class="theme_screenshot"><h2>psu_darkb</h2><img src="psu_darkb/pix/screenshot.png" /><h2>About</h2><p>psu_darkb is a three column fluid-width theme for Moodle 2.0. <h2>Tweaks</h2><p>This theme is built upon both Base and Canvas, two parent themes included in the Moodle core. If you want to modify this theme, we recommend that you first duplicate it, then rename it before making your changes. This will prevent your customized theme from being overwritten by future Moodle upgrades, and you\'ll still have the original files if you make a mess. More information on modifying themes can be found in the <a href="http://docs.moodle.org/en/Theme">MoodleDocs</a>.</p></div></div>';
-$string['configtitle'] = 'psu_darkb theme';
+$string['choosereadme'] = '<div class="clearfix"><div class="theme_screenshot"><h2>Plymouth State Green</h2><img src="psu_darkb/pix/screenshot.png" /><h2>About</h2><p>Plymouth State Green is a three column fluid-width theme for Moodle 2.0. <h2>Tweaks</h2><p>This theme is built upon both Base and Canvas, two parent themes included in the Moodle core. If you want to modify this theme, we recommend that you first duplicate it, then rename it before making your changes. This will prevent your customized theme from being overwritten by future Moodle upgrades, and you\'ll still have the original files if you make a mess. More information on modifying themes can be found in the <a href="http://docs.moodle.org/en/Theme">MoodleDocs</a>.</p></div></div>';
+$string['configtitle'] = 'Plymouth State Green Theme';
 
 $string['logo'] = 'logo';
 $string['logodesc'] = 'Change the logo of this theme by entering the URL to a new one (i.e., http://www.somesite/animage.png). As a reference, the default logo is 300px wide by 80px high. A transparent .png will work best.';
---
+++ theme/psu_darkb/version.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * version for solstice theme
+ *
+ * @package    theme
+ * @subpackage psu_darkb
+ * @copyright  John Stabinger
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die;
+
+$plugin->version = 2012042400;              // The current module version (Date: YYYYMMDDXX)
+$plugin->component = 'theme_psu_darkb';
+$plugin->maturity = MATURITY_STABLE;
