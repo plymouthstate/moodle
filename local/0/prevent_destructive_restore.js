4a95f617395101ad1c72646a6bf3399099ac5d99
nrporter <nrporter1@gmail.com>
2012-06-05 09:58:59 -0400

    Modifying PSU plugin for changing restore defaults to additionally hide them as well.

--- local/0/prevent_destructive_restore.js
+++ local/0/prevent_destructive_restore.js
@@ -10,12 +10,20 @@ M.local_psu_mod = {
         this.Y = Y;
 		this.re_dropdown = Y.one('#id_setting_course_keep_roles_and_enrolments');
 		this.gr_dropdown = Y.one('#id_setting_course_keep_groups_and_groupings');
+		this.re_fitem = Y.one('#fitem_id_setting_course_keep_roles_and_enrolments');
+		this.gr_fitem = Y.one('#fitem_id_setting_course_keep_groups_and_groupings');
+		this.oc_fitem = Y.one('#fitem_id_setting_course_overwrite_conf');
 
 		if( this.re_dropdown ) {
 			M.local_psu_mod.toggle_select( this.re_dropdown );
+			M.local_psu_mod.hide_select( this.re_fitem );
 		}
 		if( this.gr_dropdown ) {
 			M.local_psu_mod.toggle_select( this.gr_dropdown );
+			M.local_psu_mod.hide_select( this.gr_fitem );
+		}
+		if( this.oc_fitem ) {
+			M.local_psu_mod.hide_select( this.oc_fitem );
 		}
 
     },
@@ -32,5 +40,10 @@ M.local_psu_mod = {
 				Y.one('#' + to_toggle.get('id') + ' > option').set('selected','selected');
 			}
 		});
+	},
+
+	hide_select: function( to_hide ){
+		Y = this.Y;
+		Y.one('#' + to_hide.get('id')).hide();
 	}
 }
