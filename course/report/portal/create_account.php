c8b3b40f4e9801193a2864e6c7072b88bd9377fe
nrporter <nrporter1@gmail.com>
2012-08-21 08:46:59 -0400

    Modifying to fill in all necessary fields.

--- course/report/portal/create_account.php
+++ course/report/portal/create_account.php
@@ -27,36 +27,36 @@ if(md5($username . $_GET['time'] . 'monkeyballz') != $_GET['hash']){
 //load the CAS plugin moodle style
 $authcas = get_auth_plugin('cas');
 
-//check to make sure we have a valid Banner (myPlymouth) user
-if(!IDMObject::getIdentifier($username, 'login_name', 'pidm')){
-	exit('Sorry, ' . $username . ' does not appear to be a valid Banner user');
-}//end valid user check
+//check to make sure we have a valid Banner (myPlymouth) user                                                                                                                                                                                                                 
+if(!IDMObject::getIdentifier($username, 'login_name', 'pidm')){                                                                                                                                                                                                               
+	exit('Sorry, ' . $username . ' does not appear to be a valid Banner user');                                                                                                                                                                                               
+}//end valid user check                                                                                                                                                                                                                                                       
 
-//$user = $authcas->get_userinfo_asobj(addslashes($username));
-$user = new StdClass;
-$identifiers = IDMObject::getIdentifier($username, 'login_name', '*');
-unset( $identifiers['ssn'] );
-$user->username = $username;
-$user->idnumber = $identifiers['psu_id'];
-$user->firstname = $identifiers['first_name'];
-$user->lastname = $identifiers['last_name'];
-$user->email = $username . '@plymouth.edu';
-$user->icq = '';
-$user->phone1 = '';
-$user->phone2 = '';
-$user->institution = '';
-$user->department = '';
-$user->timemodified   = time();
-$user->confirmed  = 1;
-$user->auth       = 'cas';
-$user->mnethostid = $CFG->mnet_localhost_id;
-$user->address = '';
-$user->city = 'Plymouth';
-$user->country = 'US';
-$user->url = '';
-if (empty($user->lang)) {
-	$user->lang = $CFG->lang;
-}
+//$user = $authcas->get_userinfo_asobj(addslashes($username));                                                                                                                                                                                                                
+$user = new StdClass;                                                                                                                                                                                                                                                         
+$identifiers = IDMObject::getIdentifier($username, 'login_name', '*');                                                                                                                                                                                                        
+unset( $identifiers['ssn'] );                                                                                                                                                                                                                                                 
+$user->username = $username;                                                                                                                                                                                                                                                  
+$user->idnumber = $identifiers['psu_id'];                                                                                                                                                                                                                                     
+$user->firstname = $identifiers['first_name'];                                                                                                                                                                                                                                
+$user->lastname = $identifiers['last_name'];                                                                                                                                                                                                                                  
+$user->email = $username . '@plymouth.edu';                                                                                                                                                                                                                                   
+$user->icq = '';                                                                                                                                                                                                                                                              
+$user->phone1 = '';                                                                                                                                                                                                                                                           
+$user->phone2 = '';                                                                                                                                                                                                                                                           
+$user->institution = '';                                                                                                                                                                                                                                                      
+$user->department = '';                                                                                                                                                                                                                                                       
+$user->timemodified   = time();                                                                                                                                                                                                                                               
+$user->confirmed  = 1;                                                                                                                                                                                                                                                        
+$user->auth       = 'cas';                                                                                                                                                                                                                                                    
+$user->mnethostid = $CFG->mnet_localhost_id;                                                                                                                                                                                                                                  
+$user->address = '';                                                                                                                                                                                                                                                          
+$user->city = 'Plymouth';                                                                                                                                                                                                                                                     
+$user->country = 'US';                                                                                                                                                                                                                                                        
+$user->url = '';                                                                                                                                                                                                                                                              
+if (empty($user->lang)) {                                                                                                                                                                                                                                                     
+	$user->lang = $CFG->lang;                                                                                                                                                                                                                                                 
+} 
 
 global $DB;
 if ($id = $DB->insert_record('user',$user)) {
