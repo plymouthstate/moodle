f73104e9fe66da8c8cbf9ee7386f20f43d758f53
Nathan Porter <nrporter1@gmail.com>
2013-04-05 10:13:33 -0400

    Opening nav links in target blank

--- local/0/psu_nav_items.js
+++ local/0/psu_nav_items.js
@@ -5,7 +5,7 @@ M.psu_nav_items = {
 	init: function(Y) {
 		this.Y = Y;
 		if( Y.one('.block_navigation') ) {
-			Y.one('.block_navigation ul.block_tree li p.tree_item').append('<p class="tree_item branch navigation_node"><a title="myPlymouth" href="https://my.plymouth.edu">myPlymouth</a></p><p class="tree_item branch navigation_node"><a title="Plymouth.edu" href="http://www.plymouth.edu">Plymouth.edu</a></p>');
+			Y.one('.block_navigation ul.block_tree li p.tree_item').append('<p class="tree_item branch navigation_node"><a title="myPlymouth" href="https://my.plymouth.edu" target="_blank">myPlymouth</a></p><p class="tree_item branch navigation_node"><a title="Plymouth.edu" href="http://www.plymouth.edu" target="_blank">Plymouth.edu</a></p>');
 		}//end if
 	}//end init function
 }
